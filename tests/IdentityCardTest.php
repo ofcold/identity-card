28675969d8db6e48ee9ecb0a24d98a96fa283d17
Bill li <bill.li@ofcold.com>
2018-05-27 17:25:28 +0800

    K

--- tests/IdentityCardTest.php
+++ tests/IdentityCardTest.php
@@ -4,10 +4,24 @@ use Ofcold\IdentityCard\IdentityCard;
 
 class IdentityCardTest extends PHPUnit\Framework\TestCase
 {
-	public function testMake()
+	public function testMakeIdentityCardInstance()
 	{
 		$idCard = IdentityCard::make('142701198003124054');
 
 		$this->assertEquals(IdentityCard::class, get_class($idCard));
+	}
+
+	public function testMakeFalse()
+	{
+		$idCard = IdentityCard::make('');
+
+		$this->assertEquals(false, $idCard);
+	}
+
+	public function testToJson()
+	{
+		$idCard = IdentityCard::make('142701198003124054');
+
+		$this->assertJson($idCard->toJson());
 	}
 }
