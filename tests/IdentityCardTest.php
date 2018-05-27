5aa6202efc4b0e1fea45e4101bb0cf39a6465571
Bill li <bill.li@ofcold.com>
2018-05-27 19:50:05 +0800

    K

--- tests/IdentityCardTest.php
+++ tests/IdentityCardTest.php
@@ -4,24 +4,50 @@ use Ofcold\IdentityCard\IdentityCard;
 
 class IdentityCardTest extends PHPUnit\Framework\TestCase
 {
+	protected $idCard;
+
+	public function setUp()
+	{
+		$this->idCard = IdentityCard::make('142701198003124054');
+	}
+
 	public function testMakeIdentityCardInstance()
 	{
-		$idCard = IdentityCard::make('142701198003124054');
-
-		$this->assertEquals(IdentityCard::class, get_class($idCard));
+		$this->assertEquals(IdentityCard::class, get_class($this->idCard));
 	}
 
 	public function testMakeFalse()
 	{
-		$idCard = IdentityCard::make('');
+		$this->assertEquals(false, !is_a($this->idCard, IdentityCard::class));
+	}
 
-		$this->assertEquals(false, $idCard);
+	public function testArea()
+	{
+		$this->assertEquals('山西省 运城地区 运城市', $this->idCard->getArea());
+	}
+
+	public function testGender()
+	{
+		$this->assertEquals('男', $this->idCard->getGender());
+	}
+
+	public function testBirthday()
+	{
+		$this->assertEquals('1980-03-12', $this->idCard->getBirthday());
+	}
+
+	public function testAge()
+	{
+		$this->assertEquals('38', $this->idCard->getAge());
+	}
+
+	public function testConstellation()
+	{
+		$this->assertEquals('双鱼座', $this->idCard->getConstellation());
 	}
 
 	public function testToJson()
 	{
-		$idCard = IdentityCard::make('142701198003124054');
-
-		$this->assertJson($idCard->toJson());
+		$this->assertJson($this->idCard->toJson());
 	}
 }
