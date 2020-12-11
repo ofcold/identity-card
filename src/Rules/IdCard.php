2fdc5b7972333ec4542b31d0ac7923d8689553d9
Bill li <bill.li@ofcold.com>
2020-12-11 11:34:05 +0800

    formating

--- src/Rules/IdCard.php
+++ src/Rules/IdCard.php
@@ -7,27 +7,27 @@ use Ofcold\IdentityCard\IdentityCard;
 
 class IdCard implements RuleInterface
 {
-    /**
-     * Determine if the validation rule passes.
-     *
-     * @param  string $attribute
-     * @param  mixed  $value
-     *
-     * @return bool
-     */
-    public function passes($attribute, $value)
-    {
-        return IdentityCard::make($value, app()->getLocale()) !== false;
-    }
+	/**
+	 * Determine if the validation rule passes.
+	 *
+	 * @param  string $attribute
+	 * @param  mixed  $value
+	 *
+	 * @return bool
+	 */
+	public function passes($attribute, $value)
+	{
+		return IdentityCard::validate($value);
+	}
 
-    /**
-     * Get the validation error message.
-     *
-     * @return string
-     */
-    public function message()
-    {
-        return __('Please enter a valid Id Card');
-    }
+	/**
+	 * Get the validation error message.
+	 *
+	 * @return string
+	 */
+	public function message()
+	{
+		return __('Please enter a valid Id Card');
+	}
 }
 
